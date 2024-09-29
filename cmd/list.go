e31d102f04503ab4a40e8512491ebf317582e988
PWZER <pwzergo@gmail.com>
2024-09-29 17:53:37 +0800

    Fix: version sort

--- cmd/list.go
+++ cmd/list.go
@@ -5,18 +5,57 @@ package cmd
 
 import (
 	"fmt"
+	"regexp"
 	"sort"
-	"strings"
+	"strconv"
 
 	"github.com/PWZER/govm/internal"
 	"github.com/spf13/cobra"
-	"golang.org/x/mod/semver"
 )
 
 var (
 	listRemoteVersions bool
 	includeAllVersions bool
 )
+
+type goVersion struct {
+	Major      int
+	Minor      int
+	Patch      int
+	PreRelease string
+}
+
+func parseVersion(version string) goVersion {
+	v := goVersion{Major: -1, Minor: -1, Patch: -1, PreRelease: ""}
+	re := regexp.MustCompile(`^go(\d+)(\.(\d+))?(\.(\d+))?(rc\d+|beta\d+)?$`)
+	matches := re.FindStringSubmatch(version)
+	if len(matches) == 7 {
+		if len(matches[1]) > 0 {
+			v.Major, _ = strconv.Atoi(matches[1])
+		}
+		if len(matches[3]) > 0 {
+			v.Minor, _ = strconv.Atoi(matches[3])
+		}
+		if len(matches[5]) > 0 {
+			v.Patch, _ = strconv.Atoi(matches[5])
+		}
+		v.PreRelease = matches[6]
+	}
+	return v
+}
+
+func (v goVersion) Less(v2 goVersion) bool {
+	if v.Major != v2.Major {
+		return v.Major < v2.Major
+	}
+	if v.Minor != v2.Minor {
+		return v.Minor < v2.Minor
+	}
+	if v.Patch != v2.Patch {
+		return v.Patch < v2.Patch
+	}
+	return v.PreRelease < v2.PreRelease
+}
 
 var listCmd = &cobra.Command{
 	Use:          "ls",
@@ -37,8 +76,9 @@ var listCmd = &cobra.Command{
 		}
 
 		sort.Slice(versions, func(i, j int) bool {
-			return semver.Compare(strings.ReplaceAll(versions[i].Version, "go", "v"),
-				strings.ReplaceAll(versions[j].Version, "go", "v")) < 0
+			v1 := parseVersion(versions[i].Version)
+			v2 := parseVersion(versions[j].Version)
+			return v1.Less(v2)
 		})
 
 		curBinaryFile, err := internal.GetCurrentUseVersionBinaryFile()
@@ -53,6 +93,7 @@ var listCmd = &cobra.Command{
 			} else {
 				fmt.Printf("[ ] %s\n", v.Version)
 			}
+			parseVersion(v.Version)
 		}
 		return nil
 	},
